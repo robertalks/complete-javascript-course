ba3f89826c29c076579343d7fed70c62632051fb
Robert Milasan <robert@linux-source.org>
2024-01-16 17:27:35 +0100

    First commit, minor add

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,10 @@
+let js = 'Amazing';
+console.log(40 + 8 + 23 - 10);
+console.log("Joana");
+console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
