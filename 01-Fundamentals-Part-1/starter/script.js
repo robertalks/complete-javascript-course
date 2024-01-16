80d5565aca70debd789e30218861bc46f1aaf885
Robert Milasan <robert@linux-source.org>
2024-01-16 18:24:31 +0100

    Lesson 11 done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'Amazing';
 console.log(40 + 8 + 23 - 10);
 console.log("Joana");
@@ -8,3 +9,37 @@ let firstName = 'Matilda';
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = 'JM';
+let $function = 27;
+let PI = 3.1415;
+
+let County = 'Czech Republic';
+let Continent = 'Europe';
+let population = '10.000.000';
+
+console.log(County);
+console.log(Continent);
+console.log(population);
+*/
+
+let javascriptIsFun = true;
+console.log(true);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
